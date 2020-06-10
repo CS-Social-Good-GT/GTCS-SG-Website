fe983d4a94954638776ce02104f5a92bc8e66b0d
vagrawal48 <vadini@gatech.edu>
2020-06-10 16:59:46 -0500

    Basic initial setup

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,22 +1,30 @@
 import React from "react"
 import { Link } from "gatsby"
+import Styled from 'styled-components'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const Styles = Styled.section`
+
+`;
+
 const IndexPage = () => (
   <Layout>
+    <Styles>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+      <div>
+          <h1>GT CS + Social Good</h1>
+          <p>We empower students to leverage technology for social good by inspiring action, facilitating collaboration, and forging pathways towards change.</p>
+      </div>
+    
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    
+    </Styles>
   </Layout>
 )
 
